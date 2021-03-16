6ad69bcf706cd7ba21a5e7faa3b974a1ae193c6e
zongxz <772272338@qq.com>
2021-03-16 16:47:52 +0800

    1.2 测试

---
+++ src/test/java/com/zxz/tacocloud/HomeControllerTest.java
@@ -0,0 +1,36 @@
+package com.zxz.tacocloud;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)  // web 测试
+public class HomeControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;  // 注入 MockMvc
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))    // 发起对 "/" 的 GET
+
+                .andExpect(status().isOk())  // 期望得到 HTTP 200
+
+                .andExpect(view().name("home"))  // 期望得到 home 视图
+
+                .andExpect(content().string(           // 期望包含 "Welcome to ..."
+                        containsString("Welcome to...")));
+    }
+}
