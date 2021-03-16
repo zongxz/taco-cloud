eeea92fe61eab01291d5dee4bd434ccea5e202e1
zongxz <772272338@qq.com>
2021-03-16 15:46:54 +0800

    1. 新建一个 Spring 项目

---
+++ src/test/java/com/zxz/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zxz.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) // 使用 spring 的运行器
+@SpringBootTest // spring boot 测试
+class TacoCloudApplicationTests {
+
+    @Test // 测试方法
+    void contextLoads() {
+    }
+
+}
